704d2c5bf87686bbe703bb98a0a363abf8854908
ahsanali17 <ahsantime1@gmail.com>
2022-05-15 20:52:12 -0400

    Added in comments to detail whats happening in the code

--- client/index.js
+++ client/index.js
@@ -3,6 +3,8 @@ import axios from "axios";
 const ethers = require('ethers');
 const server = "http://localhost:3043";
 
+// We'll use our client to handle events and the results. We first get our information from the server index.js and we can manipulate that data further and get that to show in our front-end. 
+
 document.getElementById("public-address").addEventListener('input', async ({ target: {value} }) => {
   if(value === "") {
     document.getElementById("balance").innerHTML = 0;
@@ -10,20 +12,26 @@ document.getElementById("public-address").addEventListener('input', async ({ tar
   }
   
   const response = await axios.get(`${server}/balance/${value}`);
+  // We use the ethers library to format the hex value in our response into the amount of ETH in the inputted address
   let balance = ethers.utils.formatEther(response.data["balance"]["hex"]);
   console.log(balance);
+  // Here we set the innerHTML of our div with id balance to balance 
   document.getElementById("balance").innerHTML = balance;
   
 });
 
 document.getElementById("get-latest").addEventListener('click', async () => {
+  // Getting Axios to call the get method of our Block endpoint from our server
   const response = await axios.get(`${server}/block/`);
+  // We save our res.send({ latestBlock }) from server into latestBlock so we can use it on the client side
   const latestBlock = response.data.latestBlock;
   console.log(latestBlock);
   
+  // String interpolation with block information held within <p> tags so that we can put the html tags into block-information div
   let html = "";  
   html += `<p> Latest Block Hash: ${latestBlock.hash}</p>` 
   + `<p> Latest Block Miner: ${latestBlock.miner}</p>` 
   + `<p> Latest Block Number: ${latestBlock.number}</p>`;
+  // This is what sets the innerHTML to html for block-information div
   document.getElementById("block-information").innerHTML = html;
 });
--- server/index.js
+++ server/index.js
@@ -12,18 +12,24 @@ const provider = new ethers.providers.StaticJsonRpcProvider(process.env.ALCHEMY_
 app.use(cors());
 app.use(express.json());
 
+// We'll use this server to interact with our provider then once we get the data we need we will pass it to our client who will then pass that info to our front-end
+
 // Get the Balance of the inputted address
 app.get('/balance/:address', async (req, res) => {
   const {address} = req.params;
+  // Get the balance of the address
   const balance = await provider.getBalance(address);
   console.log(balance);
+  // Send response to client 
   res.send({ balance });
 });
 
 // Get latest ETH Block
 app.get('/block/', async (req, res) => {
+  // Here is where we use our provider methods to retrieve information from the ethereum mainnet
   const latestBlock = await provider.getBlock('latest');
   console.log("Latest Block is:" + latestBlock);
+  // We send our response to the client
   res.send({ latestBlock });
 });
 
